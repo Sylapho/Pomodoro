e14dc4863d940639d3d4851917965206efca566b
Sylapho <141580103+Sylapho@users.noreply.github.com>
2023-09-18 17:58:38 +0200

    fix: le timer peut passer du mode travail en mode pause

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,56 @@
-const departMinutes = 25;
-let temps = departMinutes * 60
+let workTime = 1500;
+let timeLeft = workTime;
+let timePause = 300;
+let isPaused = true;
+let temps = workTime/60;
 
-
-let boutonDepart = document.getElementById('debut');
+let travail = document.getElementById('travail');
+let pause = document.getElementById('pause');
+let elem = document.getElementById('chrono');
+let button = document.getElementById('debut');
 let boutonReset = document.getElementById('reset');
+let minutes = parseInt(timeLeft /60,10);
+let secondes = parseInt(timeLeft % 60, 10);
 
 boutonReset.style.display = 'none';
 
-const chronoElement = document.getElementById('chrono');
+elem.innerHTML = '25:00';
 
-boutonDepart.addEventListener('click', ()=>{
-    document.getElementById('debut').style.display = 'none'
+button.addEventListener('click', ()=>{
+    document.getElementById('debut').style.display = 'none';
     boutonReset.style.display = 'block'
-    setInterval(() => {
-        let minutes = parseInt(temps / 60, 10);
-        let secondes = parseInt(temps % 60, 10);
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        secondes = secondes < 10 ? "0" + secondes : secondes;
-      
-        chronoElement.innerText = `${minutes}:${secondes}`;
-        temps = temps <= 0 ? 0 : temps - 1;
-      }, 1000);
-})
+    travail.style.color = 'red';
+    let timerId = setInterval(countdown, 1000);
+});
+
+function countdown() {
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    secondes = secondes < 10 ? "0" + secondes : secondes;
+    if(timeLeft <= -1){
+        elem.innerHTML = 0 + ' seconds remaining';
+        if(isPaused == true){
+            isPaused = false;
+            timeLeft = timePause;
+            travail.style.color = 'black';
+            pause.style.color = 'red';
+
+        } else {
+            timeLeft = workTime;
+            isPaused = true;
+            travail.style.color = 'red';
+            pause.style.color = 'black';
+        }
+        
+    } else {
+        elem.innerHTML = `${minutes}:${secondes}`;
+        timeLeft--;
+        console.log(minutes);
+        console.log(secondes); 
+    }
+}
 
 boutonReset.addEventListener('click', ()=>{
     location.reload();
 });
+
+
