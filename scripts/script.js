ffca120439c2d183695118a0b8e0c48b1b874de0
Arthur Dubos <arthur.dubos@etu.unicaen.fr>
2023-09-18 22:40:02 +0200

    Modification pour faire fonctionner chrono

--- scripts/script.js
+++ scripts/script.js
@@ -9,43 +9,41 @@ let pause = document.getElementById('pause');
 let elem = document.getElementById('chrono');
 let button = document.getElementById('debut');
 let boutonReset = document.getElementById('reset');
-let minutes = parseInt(timeLeft /60,10);
-let secondes = parseInt(timeLeft % 60, 10);
+
 
 boutonReset.style.display = 'none';
 
 elem.innerHTML = '25:00';
 
 button.addEventListener('click', ()=>{
-    document.getElementById('debut').style.display = 'none';
-    boutonReset.style.display = 'block'
-    travail.style.color = 'red';
+    button.style.display = 'none';
+    boutonReset.style.display = 'inline-block'
+    travail.style.color = 'green';
     let timerId = setInterval(countdown, 1000);
 });
 
 function countdown() {
+    let minutes = parseInt(timeLeft /60,10);
+    let secondes = parseInt(timeLeft % 60, 10);
     minutes = minutes < 10 ? '0' + minutes : minutes;
     secondes = secondes < 10 ? "0" + secondes : secondes;
     if(timeLeft <= -1){
-        elem.innerHTML = 0 + ' seconds remaining';
         if(isPaused == true){
             isPaused = false;
             timeLeft = timePause;
-            travail.style.color = 'black';
-            pause.style.color = 'red';
+            travail.style.color = 'white';
+            pause.style.color = 'green';
 
         } else {
             timeLeft = workTime;
             isPaused = true;
-            travail.style.color = 'red';
-            pause.style.color = 'black';
+            travail.style.color = 'green';
+            pause.style.color = 'white';
         }
         
     } else {
         elem.innerHTML = `${minutes}:${secondes}`;
-        timeLeft--;
-        console.log(minutes);
-        console.log(secondes); 
+        timeLeft--; 
     }
 }
 
